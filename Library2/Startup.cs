07f1672076dd0ed7b1d304b59dfc7448537240bd
mohamed taher grati <mohamedtahergrati@yahoo.fr>
2019-05-29 14:31:43 -0400

    First commit

--- Library2/Startup.cs
+++ Library2/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using LibraryServices;
 
 namespace Library2
 {
@@ -33,6 +36,14 @@ namespace Library2
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(Configuration);
+            services.AddScoped<ILibraryAsset, LibraryAssetService>();
+            services.AddScoped<ICheckout, CheckoutService>();
+            services.AddScoped<IPatron, PatronService>();
+            services.AddScoped<ILibraryBranch, LibraryBranchService>();
+
+            services.AddDbContext<LibraryContext>(options
+                => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ LibraryData/ICheckout.cs
@@ -0,0 +1,28 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+    public interface ICheckout
+    {
+        IEnumerable<Checkout> GetAll();
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int id);
+        IEnumerable<Hold> GetCurrentHolds(int id);
+        Checkout GetById(int checkoutId);
+        Checkout GetLatestCheckout(int assetId);
+        string GetCurrentHoldPatronName(int id);
+       
+        void Add(Checkout newCheckout);
+        void CheckOutItem(int assetId, int libraryCardId);
+        void CheckInItem(int assetId);
+        void PlaceHold(int assetId, int libraryCardId);
+        
+        void MarkLost(int assetId);
+        void MarkFound(int assetId);
+        DateTime GetCurrentHoldPlaced(int id);
+        string GetCurrentCheckoutPatron(int id);
+        bool IsCheckedOut(int id);
+    }
+}
